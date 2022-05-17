7f079f5a16c3086168b7f14666f69a3714a1a8c8
GSvenny <gabrielswagner7@gmail.com>
2022-05-17 17:25:12 -0500

    reset form function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,14 @@ var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  
+
+    // check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
     // package up data as an object
     var taskDataObj = {
       name: taskNameInput,
@@ -15,6 +22,8 @@ var taskFormHandler = function (event) {
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
 };
+
+
 
 var createTaskEl = function(taskDataObj) {
     // create list item
